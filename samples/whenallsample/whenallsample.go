1b5d3e4cc1a4719acaf835d80a26c82058c80366
silver-xu <me@silverxu.com>
2018-09-06 17:09:47 +1000

    Fixing few bugs

--- samples/whenallsample/whenallsample.go
+++ samples/whenallsample/whenallsample.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"sync"
 )
 
 func main() {
@@ -16,16 +17,14 @@ func main() {
 		},
 	}
 
-	results, _ := whenAll(jobs, 0)
+	results, _ := WhenAll(jobs, 1)
 
 	for i := range results {
 		fmt.Println(i)
 	}
-
-	fmt.Scanln()
 }
 
-func whenAll(doSlice []func() (interface{}, error), numberOfWorkers int) ([]interface{}, []error) {
+func WhenAll(doSlice []func() (interface{}, error), numberOfWorkers int) ([]interface{}, []error) {
 	var results []interface{}
 	var errs []error
 
@@ -40,20 +39,7 @@ func whenAll(doSlice []func() (interface{}, error), numberOfWorkers int) ([]inte
 		numberOfWorkers = len(doSlice)
 	}
 
-	jobsChannel := make(chan func() (interface{}, error))
-
-	for i := 0; i < numberOfWorkers; i++ {
-
-		//map
-		go func(jobsChannel chan func() (interface{}, error)) {
-
-			for job := range jobsChannel {
-
-				job()
-			}
-
-		}(jobsChannel)
-	}
+	jobsChannel := make(chan func() (interface{}, error), len(doSlice))
 
 	for i := 0; i < len(doSlice); i++ {
 		jobsChannel <- doSlice[i]
@@ -61,6 +47,40 @@ func whenAll(doSlice []func() (interface{}, error), numberOfWorkers int) ([]inte
 
 	close(jobsChannel)
 
-	return results, errs
+	var worksWaitGroup sync.WaitGroup
+	resultsChannel := make(chan interface{}, len(doSlice))
+	errorsChannel := make(chan error, len(doSlice))
 
+	worksWaitGroup.Add(len(doSlice))
+
+	for i := 0; i < numberOfWorkers; i++ {
+
+		//map
+		go func(jobsChannel chan func() (interface{}, error), resultsChannel chan interface{}, errorsChannel chan error, wg *sync.WaitGroup) {
+
+			for job := range jobsChannel {
+				ret, err := job()
+
+				resultsChannel <- ret
+				errorsChannel <- err
+
+				worksWaitGroup.Done()
+			}
+
+		}(jobsChannel, resultsChannel, errorsChannel, &worksWaitGroup)
+	}
+
+	//All works completed
+	worksWaitGroup.Wait()
+
+	//close all communication channels
+	close(resultsChannel)
+	close(errorsChannel)
+
+	//reduce
+	for result := range resultsChannel {
+		results = append(results, result)
+	}
+
+	return results, errs
 }

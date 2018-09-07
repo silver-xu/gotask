7ae05d8bfb11d13744e9f5751914cddc1d241e4a
heart.wood <heart.wood@outlook.com>
2018-09-08 09:14:03 +1000

    Adding whenall to the gotask package

--- gotask.go
+++ gotask.go
@@ -1,5 +1,10 @@
 package gotask
 
+import (
+	"errors"
+	"sync"
+)
+
 //Await function equivalent to .Net Awaiter
 func Await(do func() (interface{}, error), cancel chan bool) (interface{}, error) {
 	taskChannel := make(chan interface{})
@@ -23,4 +28,65 @@ func Await(do func() (interface{}, error), cancel chan bool) (interface{}, error
 	}(do)
 
 	return <-taskChannel, <-errorChannel
+}
+
+func WhenAll(doSlice []func() (interface{}, error), numberOfWorkers int) ([]interface{}, []error) {
+	var results []interface{}
+	var errs []error
+
+	if doSlice == nil {
+		errs = append(errs, errors.New("Argument Exception"))
+
+		return results, errs
+	}
+
+	//set numberOfWorkers no to exceed the jobs to be done
+	if numberOfWorkers == 0 || numberOfWorkers > len(doSlice) {
+		numberOfWorkers = len(doSlice)
+	}
+
+	jobsChannel := make(chan func() (interface{}, error), len(doSlice))
+
+	for i := 0; i < len(doSlice); i++ {
+		jobsChannel <- doSlice[i]
+	}
+
+	close(jobsChannel)
+
+	var worksWaitGroup sync.WaitGroup
+	resultsChannel := make(chan interface{}, len(doSlice))
+	errorsChannel := make(chan error, len(doSlice))
+
+	worksWaitGroup.Add(len(doSlice))
+
+	for i := 0; i < numberOfWorkers; i++ {
+
+		//map
+		go func(jobsChannel chan func() (interface{}, error), resultsChannel chan interface{}, errorsChannel chan error, wg *sync.WaitGroup) {
+
+			for job := range jobsChannel {
+				ret, err := job()
+
+				resultsChannel <- ret
+				errorsChannel <- err
+
+				worksWaitGroup.Done()
+			}
+
+		}(jobsChannel, resultsChannel, errorsChannel, &worksWaitGroup)
+	}
+
+	//All works completed
+	worksWaitGroup.Wait()
+
+	//close all communication channels
+	close(resultsChannel)
+	close(errorsChannel)
+
+	//reduce
+	for result := range resultsChannel {
+		results = append(results, result)
+	}
+
+	return results, errs
 }
--- samples/whenallsample/whenallsample.go
+++ samples/whenallsample/whenallsample.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"sync"
+
+	"github.com/silver-xu/gotask"
 )
 
 func main() {
@@ -17,70 +17,9 @@ func main() {
 		},
 	}
 
-	results, _ := WhenAll(jobs, 1)
+	results, _ := gotask.WhenAll(jobs, 1)
 
 	for i := range results {
 		fmt.Println(i)
 	}
-}
-
-func WhenAll(doSlice []func() (interface{}, error), numberOfWorkers int) ([]interface{}, []error) {
-	var results []interface{}
-	var errs []error
-
-	if doSlice == nil {
-		errs = append(errs, errors.New("Argument Exception"))
-
-		return results, errs
-	}
-
-	//set numberOfWorkers no to exceed the jobs to be done
-	if numberOfWorkers == 0 || numberOfWorkers > len(doSlice) {
-		numberOfWorkers = len(doSlice)
-	}
-
-	jobsChannel := make(chan func() (interface{}, error), len(doSlice))
-
-	for i := 0; i < len(doSlice); i++ {
-		jobsChannel <- doSlice[i]
-	}
-
-	close(jobsChannel)
-
-	var worksWaitGroup sync.WaitGroup
-	resultsChannel := make(chan interface{}, len(doSlice))
-	errorsChannel := make(chan error, len(doSlice))
-
-	worksWaitGroup.Add(len(doSlice))
-
-	for i := 0; i < numberOfWorkers; i++ {
-
-		//map
-		go func(jobsChannel chan func() (interface{}, error), resultsChannel chan interface{}, errorsChannel chan error, wg *sync.WaitGroup) {
-
-			for job := range jobsChannel {
-				ret, err := job()
-
-				resultsChannel <- ret
-				errorsChannel <- err
-
-				worksWaitGroup.Done()
-			}
-
-		}(jobsChannel, resultsChannel, errorsChannel, &worksWaitGroup)
-	}
-
-	//All works completed
-	worksWaitGroup.Wait()
-
-	//close all communication channels
-	close(resultsChannel)
-	close(errorsChannel)
-
-	//reduce
-	for result := range resultsChannel {
-		results = append(results, result)
-	}
-
-	return results, errs
 }
